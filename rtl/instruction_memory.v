16ef270eeb6cad4cba3922f475d79174bf75fb21
Hiankitgit <148990980+Hiankitgit@users.noreply.github.com>
2026-09-18 22:52:42 +0530

    Create instruction_memory.v

---
+++ rtl/instruction_memory.v
@@ -0,0 +1,26 @@
+// ============================================================================
+// MODULE 1: INSTRUCTION MEMORY (QUARTUS ROM)
+// ============================================================================
+module instruction_memory(
+    input         clk,
+    input  [31:0] pc,
+    output [31:0] instruction_code
+);
+    wire [29:0] word_addr = pc[31:2];
+ 
+    altsyncram #(
+        .operation_mode("ROM"),
+        .width_a(32),
+        .widthad_a(10),
+        .numwords_a(1024),
+        .outdata_reg_a("UNREGISTERED"),
+        .address_aclr_a("NONE"),
+        .outdata_aclr_a("NONE"),
+        .init_file("instruction.mif"),
+        .lpm_hint("ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=iROM")
+    ) rom_inst (
+        .clock0(~clk), 
+        .address_a(word_addr[9:0]),
+        .q_a(instruction_code)
+    );
+endmodule
